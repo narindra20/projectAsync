ffca516f09590dcc9a3f0a58da240285df0ec884
narindra20 <hei.narindra@gmail.com>
2026-07-12 09:00:34 +0300

    feat: add UserRepository

---
+++ src/main/java/hei/school/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package hei.school.demo.repository;
+
+import hei.school.demo.repository.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {}
