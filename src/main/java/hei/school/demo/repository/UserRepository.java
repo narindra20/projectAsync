39f1d1f7fc45df70ce6ff0b9d261396e50036b24
narindra20 <hei.narindra@gmail.com>
2026-07-12 12:53:52 +0300

    fix: use UUID as UserRepository id type

--- src/main/java/hei/school/demo/repository/UserRepository.java
+++ src/main/java/hei/school/demo/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package hei.school.demo.repository;
 
 import hei.school.demo.repository.model.User;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {}
+public interface UserRepository extends JpaRepository<User, UUID> {}
