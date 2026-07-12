45217943e3740a95caccfe81a3cea871dd3422b6
narindra20 <hei.narindra@gmail.com>
2026-07-12 12:53:53 +0300

    fix: use UUID as CourseRepository id type

--- src/main/java/hei/school/demo/repository/CourseRepository.java
+++ src/main/java/hei/school/demo/repository/CourseRepository.java
@@ -1,8 +1,9 @@
 package hei.school.demo.repository;
 
 import hei.school.demo.repository.model.Course;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, String> {}
+public interface CourseRepository extends JpaRepository<Course, UUID> {}
