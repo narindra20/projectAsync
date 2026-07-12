9f8d159a2bf0e57ea559c73a0ff671625dd39947
narindra20 <hei.narindra@gmail.com>
2026-07-12 12:53:50 +0300

    refactor: map User to app_user table with ManyToMany courses

--- src/main/java/hei/school/demo/repository/model/User.java
+++ src/main/java/hei/school/demo/repository/model/User.java
@@ -2,16 +2,38 @@ package hei.school.demo.repository.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "app_user")
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
-  @Id private String id;
+
+  @Id private UUID id;
+
   private String firstName;
+
   private String lastName;
+
   private String userName;
+
   private String email;
+
+  @ManyToMany
+  @JoinTable(
+      name = "user_course_subscription",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "course_id"))
+  private Set<Course> courses = new HashSet<>();
 }
